b381fcae10e04d1e1d9e61fd2fbdb800fea548ce
paulcmd <choxmail.pm@gmail.com>
2021-06-05 21:13:45 -0700

    added post reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
-import jsonplaceholder from "../apis/jsonPlaceholder";
+import jsonplaceholder from '../apis/jsonPlaceholder'
 
 export const fetchPosts = () => async (dispatch) => {
-  const response = await jsonplaceholder.get("/posts");
+    const response = await jsonplaceholder.get('/posts')
 
-  dispatch({
-    type: "FETCH_POSTS",
-    payload: response,
-  });
-};
+    dispatch({
+        type: 'FETCH_POSTS',
+        payload: response.data
+    })
+}
 
 /*
 we are using the inner function, so we can do async, await.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,15 @@
 import { combineReducers } from 'redux';
-import postREducer from './postsReducer'
+import postsReducer from './postsReducer'
 
 export default combineReducers({
 posts: postsReducer
 })
+
+
+
+
+/*
+-combineReducer code on github- line 174. If nextState is not the exact same array in memory 
+as the previousState(ie we made a shadow copy and edited it) then hasChanged = true, else
+it remains false
+*/
